77976cb9dcead79b076fa0c24165294abd194289
keemdonghoo <kiingdonghoo@gmail.com>
2023-04-03 10:27:27 +0900

    마이페이지

--- MyPage.Designer.cs
+++ MyPage.Designer.cs
@@ -28,12 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MyPage";
+            label5 = new Label();
+            tab1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            tabPage3 = new TabPage();
+            tab1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(322, 21);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 15);
+            label5.TabIndex = 4;
+            label5.Text = "마이페이지";
+            // 
+            // tab1
+            // 
+            tab1.Controls.Add(tabPage1);
+            tab1.Controls.Add(tabPage2);
+            tab1.Controls.Add(tabPage3);
+            tab1.Dock = DockStyle.Bottom;
+            tab1.Location = new Point(0, 39);
+            tab1.Name = "tab1";
+            tab1.SelectedIndex = 0;
+            tab1.Size = new Size(760, 411);
+            tab1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(752, 383);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "회원정보";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(752, 383);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "내가 남긴 리뷰";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(31, 228);
+            label4.Name = "label4";
+            label4.Size = new Size(43, 15);
+            label4.TabIndex = 7;
+            label4.Text = "닉네임";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 170);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 6;
+            label3.Text = "이름";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 5;
+            label2.Text = "비밀번호";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 4;
+            label1.Text = "아이디";
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(752, 383);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "즐겨찾기";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // MyPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(760, 450);
+            Controls.Add(tab1);
+            Controls.Add(label5);
+            Name = "MyPage";
+            Text = "MyPage";
+            tab1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label label5;
+        private TabControl tab1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TabPage tabPage3;
     }
 }
