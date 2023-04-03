2bc0cc88360d7217d1934f8bd25939d6564ba3f4
leegeonjon <koka85260@gmail.com>
2023-04-03 17:14:38 +0900

    commit

--- Admin_Page.cs
+++ Admin_Page.cs
@@ -132,13 +132,14 @@ namespace TeamProject
                 // DataTable 객체 생성
                 DataTable dataTable = new DataTable();
 
-            memberView.DataSource = ds.Tables[0];
+                memberView.DataSource = ds.Tables[0];
 
-            memberView.Columns[0].ReadOnly = true;
-            memberView.Columns[0].Width = 30;
+                memberView.Columns[0].ReadOnly = true;
+                memberView.Columns[0].Width = 30;
 
-            dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill; //나머지 여백 차지해줌
-            dataGridView1.AllowUserToDeleteRows = false; // 직접 행 삭제 불가
+                memberView.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill; //나머지 여백 차지해줌
+                memberView.AllowUserToDeleteRows = false; // 직접 행 삭제 불가
+            }
         }
     }
-}
+    }
--- Main.cs
+++ Main.cs
@@ -13,6 +13,7 @@ namespace TeamProject
 {
     public partial class Main : Form
     {
+        public int logStatus { get; set; }
         const string strConn = "Server=127.0.0.1; Database=teamproject; uid=project; pwd=1234; Encrypt=false";
         SqlConnection conn;
         SqlDataReader reader;
